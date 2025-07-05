b40ae8344e8cf3fcd06dd61a06947709691297b5
Jxredkong <jkong011@e.ntu.edu.sg>
2025-07-05 17:19:35 +0800

    new project

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -42,22 +42,24 @@ const Modall = ({ project }: { project: Project }) => {
     <div className="flex items-center justify-center">
       <Modal>
         <ModalTrigger className="bg-transparent flex justify-center group/modal-btn">
-          <div
-            className="relative w-[400px] h-auto rounded-lg overflow-hidden"
-            style={{ aspectRatio: "3/2" }}
-          >
-            <Image
-              className="absolute w-full h-full top-0 left-0 hover:scale-[1.05] transition-all"
-              src={project.src}
-              alt={project.title}
-              width={300}
-              height={300}
-            />
-            <div className="absolute w-full h-1/2 bottom-0 left-0 bg-gradient-to-t from-black via-black/85 to-transparent pointer-events-none">
-              <div className="flex flex-col h-full items-start justify-end p-6">
-                <div className="text-lg text-left">{project.title}</div>
-                <div className="text-xs bg-white text-black rounded-lg w-fit px-2">
-                  {project.category}
+          <div className="p-[2px] rounded-lg bg-gradient-to-r from-brand/50 via-brand to-brand/50 shadow-[0_0_20px_rgba(139,69,255,0.5)]">
+            <div
+              className="relative w-[400px] h-auto rounded-lg overflow-hidden bg-white/70 dark:bg-black/70 backdrop-blur-sm"
+              style={{ aspectRatio: "3/2" }}
+            >
+              <Image
+                className="absolute w-full h-full top-0 left-0 hover:scale-[1.05] transition-all"
+                src={project.src}
+                alt={project.title}
+                width={300}
+                height={300}
+              />
+              <div className="absolute w-full h-1/2 bottom-0 left-0 bg-gradient-to-t from-black via-black/85 to-transparent pointer-events-none">
+                <div className="flex flex-col h-full items-start justify-end p-6">
+                  <div className="text-lg text-left">{project.title}</div>
+                  <div className="text-xs bg-white text-black rounded-lg w-fit px-2">
+                    {project.category}
+                  </div>
                 </div>
               </div>
             </div>
